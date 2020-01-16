bd8c9cbd60dbad7d498dd46b4be29ebdac0ac9e3
Adam Conder <adamdconder@gmail.com>
2020-01-16 11:24:04 +0000

    Update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -11,7 +11,7 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "play-health"                    % "3.14.0-play-26",
     "uk.gov.hmrc"       %% "play-ui"                        % "8.2.0-play-26",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "0.2.0",
-    "uk.gov.hmrc"       %% "bootstrap-play-26"              % "1.0.0",
+    "uk.gov.hmrc"       %% "bootstrap-play-26"              % "1.1.0",
     "uk.gov.hmrc"       %% "play-whitelist-filter"          % "2.0.0"
   )
 
@@ -22,7 +22,9 @@ object AppDependencies {
     "org.jsoup"                   %  "jsoup"              % "1.10.3",
     "com.typesafe.play"           %% "play-test"          % PlayVersion.current,
     "org.mockito"                 %  "mockito-all"        % "1.10.19",
-    "org.scalacheck"              %% "scalacheck"         % "1.14.0"
+    "org.scalacheck"              %% "scalacheck"         % "1.14.0",
+    "wolfendale"                  %% "scalacheck-gen-regexp" % "0.1.1",
+    "com.github.tomakehurst"      % "wiremock-standalone"      % "2.17.0"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
