91c0b431724f77ab46a0565484ef9e74b4f290b6
Jay Nabonne <jaynabonne@gmail.com>
2020-01-21 10:54:19 +0000

    TRUS-2346: Got a test to pass.

---
+++ test/base/Mocked.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package base
+
+import models.requests.IdentifierRequest
+import org.mockito.Matchers.any
+import org.mockito.Mockito.when
+import org.scalatest.mockito.MockitoSugar
+import play.api.mvc.AnyContent
+import repositories.PlaybackRepository
+
+import scala.concurrent.Future
+
+trait Mocked extends MockitoSugar {
+
+//  val registrationsRepository : RegistrationsRepository = mock[RegistrationsRepository]
+  val playbackRepository : PlaybackRepository = mock[PlaybackRepository]
+
+//  val mockSubmissionService : SubmissionService = mock[SubmissionService]
+//  val mockCreateDraftRegistrationService : CreateDraftRegistrationService = mock[CreateDraftRegistrationService]
+
+//  when(mockCreateDraftRegistrationService.create(any[OptionalRegistrationDataRequest[AnyContent]])(any()))
+//    .thenReturn(Future.successful(TestUserAnswers.draftId))
+//
+//  when(mockCreateDraftRegistrationService.create(any[IdentifierRequest[AnyContent]])(any()))
+//      .thenReturn(Future.successful(TestUserAnswers.draftId))
+//
+//  when(registrationsRepository.set(any())).thenReturn(Future.successful(true))
+  when(playbackRepository.set(any())).thenReturn(Future.successful(true))
+
+}
--- test/navigation/FakeNavigator.scala
+++ test/navigation/FakeNavigator.scala
@@ -16,12 +16,15 @@
 
 package navigation
 
-import play.api.mvc.Call
-import pages._
+import config.FrontendAppConfig
 import models.{Mode, NormalMode, UserAnswers}
+import pages._
+import play.api.mvc.Call
+import uk.gov.hmrc.auth.core.AffinityGroup
 
-class FakeNavigator(desiredRoute: Call, mode: Mode = NormalMode) extends Navigator {
-
-  override def nextPage(page: Page, mode: Mode, userAnswers: UserAnswers): Call =
-    desiredRoute
+class FakeNavigator(config: FrontendAppConfig,
+                    val desiredRoute: Call = Call("GET", "/foo"),
+                    mode: Mode = NormalMode
+                   ) extends Navigator() {
+  override def nextPage(page: Page, mode: Mode, userAnswers: UserAnswers): Call = desiredRoute
 }
