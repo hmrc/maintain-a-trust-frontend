75d190d2d6b2df65897c85ddf8145d8470927206
JoeSimmonds <JoeSimmonds@users.noreply.github.com>
2020-03-06 15:38:09 +0000

    TRUS-2164 renamed extract method

--- app/controllers/actions/RefreshedDataRetrievalAction.scala
+++ app/controllers/actions/RefreshedDataRetrievalAction.scala
@@ -60,7 +60,7 @@ class RefreshedDataRetrievalActionImpl @Inject()(val parser: BodyParsers.Default
       val submissionData = SubmissionData(utr, tvn, submissionDate, optionalAgentInformation)
 
       trustConnector.playback(utr).flatMap {
-        case Processed(playback, _) => extract(submissionData, playback)(request)
+        case Processed(playback, _) => extractAndRefreshUserAnswers(submissionData, playback)(request)
         case _ => Future.successful(Left(Redirect(routes.TrustStatusController.sorryThereHasBeenAProblem())))
       }
     }).getOrElse {
@@ -69,7 +69,7 @@ class RefreshedDataRetrievalActionImpl @Inject()(val parser: BodyParsers.Default
     }
   }
 
-  private def extract[A](data: SubmissionData, playback: GetTrust)
+  private def extractAndRefreshUserAnswers[A](data: SubmissionData, playback: GetTrust)
                         (implicit request: DataRequest[A]) : Future[Either[Result, DataRequest[A]]] = {
 
     playbackExtractor.extract(UserAnswers(request.user.internalId), playback) match {
