431bd12304b347e9d8e4f8b06ca5846431ae9ff6
Darryl Murphy <darrylmurphy@users.noreply.github.com>
2020-05-11 15:54:24 +0100

    Add protectors to text

--- test/controllers/MaintainThisTrustControllerSpec.scala
+++ test/controllers/MaintainThisTrustControllerSpec.scala
@@ -44,7 +44,7 @@ class MaintainThisTrustControllerSpec extends SpecBase {
       status(result) mustEqual OK
 
       contentAsString(result) mustEqual
-        view(utr, "settlors, trustees and beneficiaries")(fakeRequest, messages).toString
+        view(utr, "settlors, trustees, beneficiaries and protectors")(fakeRequest, messages).toString
 
       application.stop()
     }
