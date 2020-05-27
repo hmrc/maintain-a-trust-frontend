b2b48effa7fff93d384352e3554598b46a4a33cc
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2020-05-27 14:21:00 +0100

    Small changes.

--- app/connectors/TrustConnector.scala
+++ app/connectors/TrustConnector.scala
@@ -30,7 +30,7 @@ class TrustConnector @Inject()(http: HttpClient, config : FrontendAppConfig) {
 
   private def getTrustDetailsUrl(utr: String) = s"${config.trustsUrl}/trusts/$utr/trust-details"
 
-  def getTrustDetails(utr: String)(implicit hc: HeaderCarrier, ex: ExecutionContext):  Future[TrustDetails] = {
+  def getTrustDetails(utr: String)(implicit hc: HeaderCarrier, ex: ExecutionContext): Future[TrustDetails] = {
     http.GET[TrustDetails](getTrustDetailsUrl(utr))
   }
 
