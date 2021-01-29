c17286199747c8fadc2d14851ff4ac51efab3245
darrylmurphy <darrylmurphy@users.noreply.github.com>
2021-01-29 13:25:47 +0000

    Turn off auditing on logout

--- app/controllers/LogoutController.scala
+++ app/controllers/LogoutController.scala
@@ -34,6 +34,7 @@ import scala.concurrent.ExecutionContext
 class LogoutController @Inject()(
                                   appConfig: FrontendAppConfig,
                                   val controllerComponents: MessagesControllerComponents,
+                                  config: FrontendAppConfig,
                                   identify: IdentifierAction,
                                   getData: DataRetrievalAction,
                                   requireData: DataRequiredAction,
@@ -46,18 +47,22 @@ class LogoutController @Inject()(
 
       logger.info(s"[Session ID: ${utils.Session.id(hc)}] user signed out from the service, asking for feedback")
 
-      val auditData = Map(
-        "sessionId" -> Session.id(hc),
-        "event" -> "signout",
-        "service" -> "maintain-a-trust-frontend",
-        "userGroup" -> request.user.affinityGroup.toString,
-        "utr" -> request.userAnswers.utr
-      )
+      if(config.logoutAudit) {
 
-      auditConnector.sendExplicitAudit(
-        "trusts",
-        auditData
-      )
+        val auditData = Map(
+          "sessionId" -> Session.id(hc),
+          "event" -> "signout",
+          "service" -> "maintain-a-trust-frontend",
+          "userGroup" -> request.user.affinityGroup.toString,
+          "utr" -> request.userAnswers.utr
+        )
+
+        auditConnector.sendExplicitAudit(
+          "trusts",
+          auditData
+        )
+
+      }
 
       Redirect(appConfig.logoutUrl).withSession(session = ("feedbackId", Session.id(hc)))
   }
