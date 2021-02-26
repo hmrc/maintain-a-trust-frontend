a4e8323300e9d6c6df789558548d595c4d8938ff
carrielies <carrielies@users.noreply.github.com>
2021-02-26 12:42:19 +0000

    TRUS-3792 Update Trust Closed Content

--- test/views/status/TrustClosedViewSpec.scala
+++ test/views/status/TrustClosedViewSpec.scala
@@ -35,7 +35,7 @@ class TrustClosedViewSpec extends ViewBehaviours {
       "utr",
       utr,
       "p1",
-      "p2",
+      "UTR.p2",
       "contact.link",
       "p3",
       "return.link"
@@ -55,7 +55,7 @@ class TrustClosedViewSpec extends ViewBehaviours {
       "urn",
       urn,
       "p1",
-      "p2",
+      "URN.p2",
       "contact.link",
       "p3",
       "return.link"
