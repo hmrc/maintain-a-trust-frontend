d49d7e393dcdd14783eab24dbae6735352a19eaa
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2021-05-10 09:02:36 +0100

    Added log to show when user has underlying 4MLD data and needs to answer express-trust question and confirm taxable before declaring.

--- app/controllers/TrustStatusController.scala
+++ app/controllers/TrustStatusController.scala
@@ -200,6 +200,7 @@ class TrustStatusController @Inject()(
 
   private def routeAfterExtraction[A](answers: UserAnswers, fromVerify: Boolean)(implicit request: OptionalDataRequest[A]): Result = {
     if (answers.trustMldStatus == Underlying4mldTrustIn5mldMode) {
+      logger.info(s"[Session ID: ${Session.id(hc)}] underlying data is 4MLD. Need to answer express-trust question.")
       Redirect(controllers.transition.routes.ExpressTrustYesNoController.onPageLoad())
     } else {
       (request.user.affinityGroup, fromVerify) match {
