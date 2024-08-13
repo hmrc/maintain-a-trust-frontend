c2655e2840eef005d9904e16f08044ee0ecf705b
zmdnoor <71127865+zmdnoor@users.noreply.github.com>
2024-08-13 12:30:18 +0100

    DDCE-5411 Content update on POR page (#423)
    * DDCE-5411 Content update on POR page
    
    * [DDCE-5411] Temp commit
    
    * [DDCE-5411] Cleaned up un-needed if statement
    
    * Update tests
    
    * Dependency updates
    
    * Amend welsh content
    
    * Address comments
    
    ---------
    
    Co-authored-by: Matthew Ward <96430760+matthewwardcap@users.noreply.github.com>

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 object AppDependencies {
 
   val bootstrapVersion = "8.6.0"
-  val mongoVersion     = "1.9.0"
+  val mongoVersion     = "2.2.0"
 
   private val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % mongoVersion,
@@ -11,7 +11,7 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "9.11.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "2.0.0",
     "uk.gov.hmrc"       %% "domain-play-30"                        % "9.0.0",
-    "org.typelevel"     %% "cats-core"                             % "2.10.0"
+    "org.typelevel"     %% "cats-core"                             % "2.12.0"
   )
 
   private val test: Seq[ModuleID] = Seq(
@@ -19,7 +19,7 @@ object AppDependencies {
     "uk.gov.hmrc.mongo"    %% "hmrc-mongo-test-play-30" % mongoVersion,
     "org.scalatestplus"    %% "scalacheck-1-17"         % "3.2.18.0",
     "io.github.wolfendale" %% "scalacheck-gen-regexp"   % "1.1.0",
-    "org.jsoup"             % "jsoup"                   % "1.17.2"
+    "org.jsoup"             % "jsoup"                   % "1.18.1"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID]      = compile ++ test
