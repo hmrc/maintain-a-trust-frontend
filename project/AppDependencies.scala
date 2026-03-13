58661f299e37361efc762dd9d44934b4c3025906
mithra-balakrishnan <177737570+mithra-balakrishnan@users.noreply.github.com>
2026-03-13 12:09:16 +0000

    DDCE-8244 - update deps

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
   private val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % mongoVersion,
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.27.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.32.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.5.0",
     "uk.gov.hmrc"       %% "domain-play-30"                        % "11.0.0",
     "org.typelevel"     %% "cats-core"                             % "2.13.0"
@@ -17,7 +17,7 @@ object AppDependencies {
   private val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"          %% "bootstrap-test-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo"    %% "hmrc-mongo-test-play-30" % mongoVersion,
-    "org.scalatestplus"    %% "scalacheck-1-18"         % "3.2.19.0",
+    "org.scalatestplus"    %% "scalacheck-1-19"         % "3.2.19.0",
     "io.github.wolfendale" %% "scalacheck-gen-regexp"   % "1.1.0"
   ).map(_ % Test)
 
