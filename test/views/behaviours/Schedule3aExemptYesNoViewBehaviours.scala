9d334eb07c4576cbb15408400bef3a233e83bb2a
mithra-balakrishnan <177737570+mithra-balakrishnan@users.noreply.github.com>
2026-07-02 10:21:44 +0100

    DDCE-8077 - updating for /maintain-a-trust/has-schedule-3a-exemption

--- test/views/behaviours/Schedule3aExemptYesNoViewBehaviours.scala
+++ test/views/behaviours/Schedule3aExemptYesNoViewBehaviours.scala
@@ -40,7 +40,7 @@ trait Schedule3aExemptYesNoViewBehaviours extends QuestionViewBehaviours[Boolean
           val legends = doc.getElementsByTag("legend")
           legends.size     mustBe 1
           legends.first.text must include(messages(s"$messageKeyPrefix.subheading", args: _*))
-
+          legends.hasClass("govuk-heading-m") mustBe true
           hintTextPrefix.map { pref =>
             doc.getElementsByClass("govuk-hint").first.text must include(messages(s"$pref.hint"))
           }
